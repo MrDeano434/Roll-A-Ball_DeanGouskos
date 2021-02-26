ad0cba8c2de7f97a47305c3091490a2b29da579e
MrDeano434 <cgouskos@myJMC.edu.au>
2021-02-26 12:00:24 +1100

    Pickup Logic
    - Added Pickup prefabs
    - Added Rotator script
    - Added win condition
    - Added score collection
    - Added CameraControl script

---
+++ Roll-A-Ball/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //transform.position = player.transform.position; 
+        //this code does not contain an offset and will positon inside the ball.
+        transform.position = player.transform.position + offset;
+    }
+}
--- Roll-A-Ball/Assets/Scripts/PlayerMovement.cs
+++ Roll-A-Ball/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float jumpForce;
     public float moveSpeed;
+    private int score = 0;
 
     void Start()
     {
@@ -21,12 +22,32 @@ public class PlayerMovement : MonoBehaviour
             //rb.AddForce(Vector3.up * jumpForce,);     Another way to add force
 
             rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
-
         }
 
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
         rb.AddForce(x, 0, z);
 
+        if(score >= 8)
+        {
+            print("Congrats, you yeeted dem cubes");
+        }
     }
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        //int score = 0; this is a local function and isnt saved/recognised outside THIS OnTriggerEnter
+        //print("woo");
+        if(otherObject.tag == "Pickup")
+        {
+            Destroy(otherObject.gameObject);
+            //print("Yeet");
+            score += 1;
+            print("Score = " + score);
+            //score = score + 1;
+            //score++;
+            //All three will add 1 to the score, score++ will only ever add 1
+        }
+    }
+
 }
